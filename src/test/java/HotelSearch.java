5555c64917ec7758eaab3f3c261788bcd93862b0
mHonc <michal.honc123@gmail.com>
2023-10-04 18:20:44 +0200

    Added simple hotel search

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -19,8 +19,21 @@ public class HotelSearch {
         driver.findElement(By.id("s2id_autogen8")).click();
         driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+        //first method
+        driver.findElement(By.xpath("//input[@class='form input-lg dpd1']")).sendKeys("05/12/2023");
+        driver.findElement(By.xpath("//input[@class='form input-lg dpd2']")).sendKeys("12/12/2023");
+        //second method
+        //driver.findElement(By.xpath("//input[@class='form input-lg dpd2']")).click();
+        //driver.findElements(By.xpath("//td[@class='day ' and text()='16']")).stream().filter(el -> el.isDisplayed()).findFirst().ifPresent(el -> el.click());
 
-        driver.quit();
+        driver.findElement(By.id("travellersInput")).click();
+        driver.findElement(By.id("adultInput")).clear();
+        driver.findElement(By.id("adultInput")).sendKeys("4");
+        driver.findElement(By.id("childInput")).clear();
+        driver.findElement(By.id("childInput")).sendKeys("2");
+        driver.findElement(By.xpath("//button[@class='btn btn-lg btn-block btn-primary pfb0 loader']")).click();
+
+        //driver.quit();
     }
 
 }
