3943cb618254aae333a69eda98d3ea2317e2d0d3
mHonc <michal.honc123@gmail.com>
2023-10-04 17:02:23 +0200

    Added picking city

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class HotelSearch {
+    @Test
+    public void searchHotel(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://www.kurs-selenium.pl/demo/m-hotels");
+
+        driver.findElement(By.id("s2id_autogen8")).click();
+        driver.findElement(By.xpath("//*[@id=\"select2-drop\"]/div/input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+
+        driver.quit();
+    }
+
+}
