83a71aacfe107262f5d1f61997c2a60c43225117
mHonc <michal.honc123@gmail.com>
2023-10-20 15:19:21 +0200

    Added logger

--- src/test/java/seleniumdemo/pages/HotelSearchPage.java
+++ src/test/java/seleniumdemo/pages/HotelSearchPage.java
@@ -1,5 +1,7 @@
 package seleniumdemo.pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -43,37 +45,49 @@ public class HotelSearchPage {
 
     private WebDriver driver;
 
+    private static final Logger logger = LogManager.getLogger();
+
     public HotelSearchPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
     public void setCity(String city) {
+        logger.info("Setting city " + city);
         searchHotelSpan.click();
         searchHotelInput.sendKeys(city);
         String hotelMatch = "//span[@class='select2-match' and text()='" + city + "']";
         driver.findElement(By.xpath(hotelMatch)).click();
+        logger.info("Setting city done");
     }
 
     public void setDate(String checkIn, String checkOut) {
+        logger.info("Setting date");
         checkInInput.sendKeys(checkIn);
         checkOutInput.sendKeys(checkOut);
+        logger.info("Setting date done");
     }
 
     public void setTravelersCount(int adults, int childs) {
+        logger.info("Setting travellers count");
         travellersInput.click();
         adultInput.clear();
         childInput.clear();
         adultInput.sendKeys(String.valueOf(adults));
         childInput.sendKeys(String.valueOf(childs));
+        logger.info("Setting travellers count done");
     }
 
     public void performSearch() {
+        logger.info("Performing search");
         searchButton.click();
+        logger.info("Performing search done");
     }
 
     public void openSignUpPage() {
+        logger.info("Opening sign up page");
         accountLink.stream().filter(WebElement::isDisplayed).findFirst().ifPresent(WebElement::click);
         signUpPageButton.get(1).click();
+        logger.info("Opening sign up page done");
     }
 }
