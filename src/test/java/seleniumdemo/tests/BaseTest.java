6ad9f492dfec58957ce4c24b455bf685a8057746
mHonc <michal.honc123@gmail.com>
2023-10-20 17:38:17 +0200

    Added config.properties

--- src/test/java/seleniumdemo/tests/BaseTest.java
+++ src/test/java/seleniumdemo/tests/BaseTest.java
@@ -7,14 +7,15 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import seleniumdemo.utils.DriverFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
 
     @BeforeMethod
-    public void setup() {
-        driver = DriverFactory.getDriver("");
+    public void setup() throws IOException {
+        driver = DriverFactory.getDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("http://www.kurs-selenium.pl/demo/m-hotels");
