60dad702f7a7228a7de3cb3baf7f62fd7e07f2b6
mHonc <michal.honc123@gmail.com>
2023-10-23 17:49:17 +0200

    loading file from excel data provider

--- src/test/java/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/seleniumdemo/tests/HotelSearchTest.java
@@ -3,10 +3,13 @@ package seleniumdemo.tests;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import seleniumdemo.pages.HotelSearchPage;
 import seleniumdemo.pages.ResultsPage;
+import seleniumdemo.utils.ExcelReader;
 
+import java.io.IOException;
 import java.util.List;
 
 
@@ -42,6 +45,31 @@ public class HotelSearchTest extends BaseTest {
 
         ResultsPage resultsPage = new ResultsPage(driver);
         Assert.assertEquals(resultsPage.getResultHeading(), "No Results Found");
+    }
+
+    @Test(dataProvider = "getData")
+    public void searchHotelTestWithDataProvider(String city, String hotel) {
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setCity(city);
+        hotelSearchPage.setDate("05/12/2023", "12/12/2023");
+        hotelSearchPage.setTravelersCount(4, 2);
+        hotelSearchPage.performSearch();
+
+        ResultsPage resultsPage = new ResultsPage(driver);
+
+        List<String> hotels = resultsPage.getHotelList();
+        hotels.forEach(el -> System.out.println(el));
+        //Assert.assertEquals(hotels.size(), 4);
+
+        List<WebElement> elementsCities = driver.findElements(By.xpath("//a[@class='go-right ellipsisFIX go-text-right mob-fs14']"));
+        for (WebElement el : elementsCities) {
+            Assert.assertEquals(el.getText(), city.toLowerCase());
+        }
+    }
+
+    @DataProvider
+    public Object[][] getData() throws IOException {
+        return ExcelReader.readExcel("TestData.xlsx");
     }
 
 }
